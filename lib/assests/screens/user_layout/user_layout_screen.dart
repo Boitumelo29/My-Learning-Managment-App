de9a54151e2e66ee428087af0906d1fc45c3c14d
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-05 20:00:05 +0200

    update

---
+++ lib/assests/screens/user_layout/user_layout_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UserLayoutScreen extends StatefulWidget {
+  const UserLayoutScreen({super.key});
+
+  @override
+  State<UserLayoutScreen> createState() => _UserLayoutScreenState();
+}
+
+class _UserLayoutScreenState extends State<UserLayoutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(),
+      ),
+    );
+  }
+}
