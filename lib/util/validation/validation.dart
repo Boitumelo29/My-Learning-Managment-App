f1c3624a5b9a58867f256838ac8a229af4ccbedd
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-06 16:29:30 +0200

    update

--- lib/util/validation/validation.dart
+++ lib/util/validation/validation.dart
@@ -1,9 +1,19 @@
 class Validation {
   static String? emailValidation(String value) {
     if (value.isEmpty ||
-        !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        !RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
             .hasMatch(value)) {
       return "Email is not valid";
+    }
+    return null;
+  }
+
+  static String? passwordValidation(String value) {
+    if (value.isEmpty) {
+      return "Email is not valid";
+    } else if (value =< 6) {
+      return "Please enter 6+ digits";
     }
     return null;
   }
