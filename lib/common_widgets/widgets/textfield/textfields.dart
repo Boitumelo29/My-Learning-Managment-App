373a8d1d6a7531e25390eb656dbf6a6a77c7ea92
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-06 10:21:27 +0200

    update

--- lib/common_widgets/widgets/textfield/textfields.dart
+++ lib/common_widgets/widgets/textfield/textfields.dart
@@ -53,22 +53,39 @@ class LongTextFieldForm extends StatefulWidget {
 }
 
 class _LongTextFieldFormState extends State<LongTextFieldForm> {
-  bool passwordVisible= false;
+  bool passwordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    passwordVisible = true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-        controller: widget.controller,
-        decoration: InputDecoration(
-          suffixIcon: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.remove_red_eye),
-          ),
-          // suffix: suffexIcon ? Icon(Icons.remove_red_eye) : Icon(Icons.remove_red_eye),
-          hintText: widget.hintText,
-          labelText: widget.labelText,
-          border: OutlineInputBorder(
-              borderSide: const BorderSide(color: Colors.red),
-              borderRadius: BorderRadius.circular(30)),
-        ));
+      obscureText: passwordVisible,
+      controller: widget.controller,
+      decoration: InputDecoration(
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              passwordVisible = !passwordVisible;
+            });
+          },
+          icon: Icon(passwordVisible
+              ? Icons.visibility_sharp
+              : Icons.visibility_off_sharp),
+        ),
+        // suffix: suffexIcon ? Icon(Icons.remove_red_eye) : Icon(Icons.remove_red_eye),
+        hintText: widget.hintText,
+        labelText: widget.labelText,
+        border: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.red),
+            borderRadius: BorderRadius.circular(30)),
+      ),
+      keyboardType: TextInputType.visiblePassword,
+      textInputAction: TextInputAction.done,
+    );
   }
 }
