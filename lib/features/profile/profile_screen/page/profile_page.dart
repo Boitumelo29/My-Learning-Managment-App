3a6c7a56050a6a68be8354e54df8abb325a793e0
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-21 15:51:34 +0200

    update

--- lib/features/profile/profile_screen/page/profile_page.dart
+++ lib/features/profile/profile_screen/page/profile_page.dart
@@ -6,6 +6,26 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List profileScreenDet = [
+      {
+        "icon": const Icon(Icons.person),
+        "name": "Edit Profile",
+        "navigation": MaterialPageRoute(
+            builder: (BuildContext context) => const ExamplePage())
+      }
+    ];
+
     return AppBarScreen(children: []);
+  }
+}
+
+class ExamplePage extends StatelessWidget {
+  const ExamplePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
   }
 }
