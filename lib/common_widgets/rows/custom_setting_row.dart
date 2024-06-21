2ceb43d0f9a9329cd5941307a291423902f2bec1
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-21 16:10:53 +0200

    udpate

--- lib/common_widgets/rows/custom_setting_row.dart
+++ lib/common_widgets/rows/custom_setting_row.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CustomSettingRow extends StatelessWidget {
-  const CustomSettingRow({super.key});
+  final IconData icon;
+  final String title;
+  final VoidCallback onTapped;
+
+  const CustomSettingRow(
+      {super.key,
+      required this.icon,
+      required this.title,
+      required this.onTapped});
 
   @override
   Widget build(BuildContext context) {
