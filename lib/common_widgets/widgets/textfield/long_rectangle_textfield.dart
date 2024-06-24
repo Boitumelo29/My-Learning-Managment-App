1f414e0580c4f99eb16fe738afd6828c75f3bd48
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-24 15:54:21 +0200

    update

--- lib/common_widgets/widgets/textfield/long_rectangle_textfield.dart
+++ lib/common_widgets/widgets/textfield/long_rectangle_textfield.dart
@@ -1,1 +1,35 @@
 import 'package:flutter/material.dart';
+
+class LongRectangleTextField extends StatefulWidget {
+  final TextEditingController? controller;
+  final String hintText;
+  final String labelText;
+  final bool showPrefixIcon;
+  final IconData? prefixIcon;
+  final dynamic validator;
+  final Function(String) onChanged;
+
+  const LongRectangleTextField(
+      {super.key,
+      this.controller,
+      required this.hintText,
+      required this.labelText,
+      required this.showPrefixIcon,
+      this.prefixIcon,
+      this.validator,
+      required this.onChanged});
+
+  @override
+  State<LongRectangleTextField> createState() => _LongRectangleTextFieldState();
+}
+
+class _LongRectangleTextFieldState extends State<LongRectangleTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator:  widget.validator,
+      onChanged: widget.onChanged,
+      controller: widget.controller,
+    );
+  }
+}
