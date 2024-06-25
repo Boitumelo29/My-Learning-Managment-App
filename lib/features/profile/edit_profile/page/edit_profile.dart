877847df7f5158afffd34d9408ffe9459be95120
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-25 10:43:50 +0200

    update

--- lib/features/profile/edit_profile/page/edit_profile.dart
+++ lib/features/profile/edit_profile/page/edit_profile.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mylearning/common_widgets/screens/appBar_layout/app_bar_screen.dart';
 import 'package:mylearning/common_widgets/sized_box/sized_space.dart';
+import 'package:mylearning/common_widgets/widgets/buttons/long_button.dart';
 import 'package:mylearning/common_widgets/widgets/buttons/long_rectangle_button.dart';
 import 'package:mylearning/common_widgets/widgets/textfield/long_rectangle_textfield.dart';
 import 'package:mylearning/common_widgets/widgets/textfield/textfields.dart';
@@ -27,20 +28,24 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     int selectedIteIndex = 0;
     return AppBarScreen(title: "Edit Profile", shouldScroll: true, children: [
       Center(
-        child: Column(
-          children: [
+      
             Stack(
+              alignment: Alignment.center,
               children: [
                 const Icon(
                   Icons.person,
                   size: 60,
                 ),
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.camera,
-                      size: 10,
-                    )),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.camera,
+                        size: 10,
+                      )),
+                ),
                 SizedBox(
                     height: 100,
                     width: 100,
@@ -59,24 +64,22 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       ),
       const SizedSpace(),
       LongTextFieldForm(
-        focusNode: null,
         hintText: Strings.username,
         labelText: Strings.username,
         showPrefixIcon: true,
         onChanged: (value) {},
         showSuffixIcon: false,
-        validator: () {},
+        validator: (value) {},
         obsureText: false,
       ),
       const SizedSpace(),
       LongTextFieldForm(
-        focusNode: null,
         hintText: Strings.username,
         labelText: Strings.username,
         showPrefixIcon: true,
         onChanged: (value) {},
         showSuffixIcon: false,
-        validator: () {},
+        validator: (value) {},
         obsureText: false,
       ),
       const SizedSpace(),
@@ -94,7 +97,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
         }),
       ),
       const SizedSpace(),
-      LongRectangleButton(onTap: () {}, title: "Save")
+      LongButton(onTap: () {}, title: "Save")
     ]);
   }
 }
