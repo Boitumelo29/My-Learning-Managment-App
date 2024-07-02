0a7295e14ccea0b2cfc86b36b805031142c1b459
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-02 16:59:57 +0200

    update

--- lib/features/profile/faq_screen/screen/faq_screen.dart
+++ lib/features/profile/faq_screen/screen/faq_screen.dart
@@ -1,15 +1,36 @@
+import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:flutter/material.dart';
+import 'package:mylearning/common_widgets/screens/appBar_layout/app_bar_screen.dart';
 
-class  extends StatefulWidget {
-  const ({super.key});
+class FAQScreen extends StatefulWidget {
+  const FAQScreen({super.key});
 
   @override
-  State<> createState() => _State();
+  State<FAQScreen> createState() => _FAQScreenState();
 }
 
-class _State extends State<> {
+class _FAQScreenState extends State<FAQScreen> {
+  final GlobalKey<ExpansionTileCardState> cardA = GlobalKey();
+  final GlobalKey<ExpansionTileCardState> cardB = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppBarScreen(
+      title: "Frequently Asked Questions",
+      shouldScroll: true,
+      children: [
+        ListView(
+          children: [
+            ExpansionTileCard(
+              key: cardB,
+              leading: const CircleAvatar(
+                child: Text("A"),
+              ),
+              title: const Text("Who are we"),
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
