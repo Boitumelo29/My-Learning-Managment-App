19a239f4caefaeff1271b3137d455145417bd778
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-18 09:50:44 +0200

    update

--- lib/features/upcoming_events/upcoming_event_screen/pages/upcoming_screen.dart
+++ lib/features/upcoming_events/upcoming_event_screen/pages/upcoming_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mylearning/common_widgets/screens/appBar_layout/app_bar_screen.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class UpcomingEvents extends StatefulWidget {
   const UpcomingEvents({super.key});
@@ -9,8 +10,25 @@ class UpcomingEvents extends StatefulWidget {
 }
 
 class _UpcomingEventsState extends State<UpcomingEvents> {
+  CalendarFormat _calendarFormat = CalendarFormat.week;
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+  double _hoursWorked = 6;
+  String _workDetails = "";
+
+// List<Task> _tasks = [];
+
   @override
   Widget build(BuildContext context) {
-    return AppBarScreen(shouldScroll: true, title: "", children: []);
+    return AppBarScreen(
+        shouldScroll: true,
+        title: "Upcoming Events",
+        children: [
+          TableCalendar(
+            focusedDay: DateTime.utc(2010, 10, 16),
+            firstDay: _focusedDay,
+            lastDay: DateTime.utc(2030),
+          )
+        ]);
   }
 }
