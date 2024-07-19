b4ff4e513d3452a6c1cc9994dc1399b4fafba444
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-19 11:49:06 +0200

    update

--- lib/features/timetable/timetable_screen/page/timetable_screen.dart
+++ lib/features/timetable/timetable_screen/page/timetable_screen.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class TimetablePage extends StatefulWidget {
+  const TimetablePage({super.key});
+
+  @override
+  State<TimetablePage> createState() => _TimetablePageState();
+}
+
+class _TimetablePageState extends State<TimetablePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
