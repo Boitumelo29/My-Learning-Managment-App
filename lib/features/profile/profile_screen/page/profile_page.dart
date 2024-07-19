a6947319f790dcb5689ac20006acd91b87ac7c55
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-19 13:06:24 +0200

    update

--- lib/features/profile/profile_screen/page/profile_page.dart
+++ lib/features/profile/profile_screen/page/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mylearning/common_widgets/rows/custom_setting_row.dart';
+import 'package:mylearning/common_widgets/screens/appBar_layout/app_bar_screen.dart';
 import 'package:mylearning/common_widgets/screens/appBar_layout/no_app_bar_screen.dart';
 import 'package:mylearning/common_widgets/screens/bottom_sheet_layout/bottom_sheet_modal.dart';
 import 'package:mylearning/common_widgets/sized_box/sized_space.dart';
@@ -52,33 +53,37 @@ class ProfilePage extends StatelessWidget {
       // },
     ];
 
-    return NoAppBarScreen(shouldScroll: false, children: [
-      const Column(
-        children: [Icon(Icons.person), SizedSpace(), Text("Name")],
-      ),
-      const SizedSpace(
-        height: 40,
-      ),
-      ListView.separated(
-        separatorBuilder: (context, index) {
-          return const Divider();
-        },
-        itemCount: profileScreenDet.length,
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemBuilder: (context, index) {
-          var itm = profileScreenDet[index] as Map;
-          return CustomSettingRow(
-              icon: itm['icon'].icon,
-              title: itm['name'],
-              onTapped: () {
-                if (itm['navigation'] != null) {
-                  Navigator.push(context, itm['navigation']);
-                }
-              });
-        },
-      )
-    ]);
+    return AppBarScreen(
+        shouldScroll: false,
+        title: 'Profile',
+        shouldHaveFloatingButton: false,
+        children: [
+          const Column(
+            children: [Icon(Icons.person), SizedSpace(), Text("Name")],
+          ),
+          const SizedSpace(
+            height: 40,
+          ),
+          ListView.separated(
+            separatorBuilder: (context, index) {
+              return const Divider();
+            },
+            itemCount: profileScreenDet.length,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              var itm = profileScreenDet[index] as Map;
+              return CustomSettingRow(
+                  icon: itm['icon'].icon,
+                  title: itm['name'],
+                  onTapped: () {
+                    if (itm['navigation'] != null) {
+                      Navigator.push(context, itm['navigation']);
+                    }
+                  });
+            },
+          )
+        ]);
   }
 }
 
