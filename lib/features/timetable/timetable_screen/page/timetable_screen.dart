d5f83fbab6770939c15e09ea56f6c638711e7a43
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-19 12:18:40 +0200

    update

--- lib/features/timetable/timetable_screen/page/timetable_screen.dart
+++ lib/features/timetable/timetable_screen/page/timetable_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_planner/time_planner.dart';
 
 class TimetablePage extends StatefulWidget {
   const TimetablePage({super.key});
@@ -10,6 +11,20 @@ class TimetablePage extends StatefulWidget {
 class _TimetablePageState extends State<TimetablePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: TimePlanner(
+          startHour: 6,
+          endHour: 23,
+          use24HourFormat: false,
+          setTimeOnAxis: false,
+          headers: [
+            TimePlannerTitle(title: "Monday"),
+          ],
+          tasks: [],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){},tooltip: "Add Task",),
+    );
   }
 }
