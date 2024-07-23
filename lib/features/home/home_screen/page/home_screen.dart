8afdb3a96e4e665807f1391c126fa855c41667ca
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-23 10:37:43 +0200

    upate

--- lib/features/home/home_screen/page/home_screen.dart
+++ lib/features/home/home_screen/page/home_screen.dart
@@ -14,24 +14,54 @@ class HomePage extends StatefulWidget {
 class _HomeScreenState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-   //QOTDataService.fetchData();
+    //QOTDataService.fetchData();
     return AppBarScreen(
       shouldBeCentered: true,
       shouldScroll: false,
       shouldHaveFloatingButton: false,
       title: 'Home Screen',
       children: [
-        const Text("Hello world"),
+        const Text("Hello UserName, welcome back!"),
         Container(
-          height: 200,
-          width: 200,
+          height: 100,
+          width: 400,
           decoration: BoxDecoration(
               color: Colors.red, borderRadius: BorderRadius.circular(20)),
-          child: Text("The Qoute of the day"),
+          child: const Center(child: Column(
+            children: [
+              Icon(Icons.format_quote_sharp),
+              Text("The Quote of the day"),
+            ],
+          )),
         ),
-        Text("Quote of the day in a container"),
-        Text("Upcoming events"),
-        Text("Jobs"),
+        const Text("Upcoming events"),
+        Container(
+          height: 100,
+          width: 400,
+          decoration: BoxDecoration(
+              color: Colors.red, borderRadius: BorderRadius.circular(20)),
+          child: const Center(child: Column(
+            children: [
+              Icon(Icons.calendar_month),
+              Text("upcoming events"),
+              Text("when pressed it directs you to the upcoming events page // if none then its says no upcoming events"),
+            ],
+          )),
+        ),
+        const Text("What is in the market"),
+        Container(
+          height: 100,
+          width: 400,
+          decoration: BoxDecoration(
+              color: Colors.red, borderRadius: BorderRadius.circular(20)),
+          child: const Center(child: Column(
+            children: [
+              Icon(Icons.search),
+              Text("random job posts for students"),
+              Text("when pressed it directs you to the job spec details. we can also just redirect them to the job site"),
+            ],
+          )),
+        ),
       ],
     );
   }
