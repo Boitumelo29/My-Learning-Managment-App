36ed81ed59c63be8c6b8f98df192b6f567a6c021
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-24 11:34:30 +0200

    update

--- lib/features/timetable/timetable_screen/page/timetable_screen.dart
+++ lib/features/timetable/timetable_screen/page/timetable_screen.dart
@@ -26,6 +26,8 @@ class _TimetablePageState extends State<TimetablePage> {
     "Saturday",
     "Sunday",
   ];
+  Color? selectedTaskColor;
+  String? selectedDayOfTheWeek;
 
   @override
   Widget build(BuildContext context) {
@@ -38,9 +40,9 @@ class _TimetablePageState extends State<TimetablePage> {
         padding: const EdgeInsets.all(8.0),
         child: Center(
           child: TimePlanner(
-            startHour: 6,
+            startHour: 5,
             endHour: 23,
-            use24HourFormat: false,
+            use24HourFormat: true,
             setTimeOnAxis: false,
             headers: const [
               TimePlannerTitle(title: "Monday"),
@@ -119,121 +121,125 @@ class _TimetablePageState extends State<TimetablePage> {
             padding: const EdgeInsets.only(top: 10, right: 30, left: 30),
             child: SizedBox(
               height: 450,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const Center(
-                      child: Icon(
-                        Icons.drag_handle,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    const Center(
-                      child: Text(
-                        "Add a Task",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: LongTextFieldForm(
-                        onChanged: (value) {},
-                        hintText: 'Enter Task',
-                        labelText: 'Enter task',
-                        showSuffixIcon: false,
-                        showPrefixIcon: true,
-                        prefixIcon: Icons.pending_actions,
-                        validator: (value) {
-                          Validation.usernameValidation(value);
-                        },
-                        obsureText: false,
-                      ),
-                    ),
-                    const SizedSpace(),
-                    const Text("Choose a date & Time",
-                        style: TextStyle(fontSize: 15)),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          IconsContainer(
-                            onPressed: () {
-                              showDatePicker(context);
-                            },
-                            title: 'Date',
-                            icon: Icons.date_range,
+              child: StatefulBuilder(
+                builder: (BuildContext context, StateSetter setState) {
+                  return Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const Center(
+                          child: Icon(
+                            Icons.drag_handle,
+                            color: Colors.grey,
                           ),
-                          IconsContainer(
-                              onPressed: () {
-                                showTimePicker(context);
-                              },
-                              title: "Time",
-                              icon: Icons.timelapse),
-                        ],
-                      ),
-                    ),
-                    const Text("Task Type & Duration"),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          IconsContainer(
-                              onPressed: () {
-                                showTaskPicker(context);
-                              },
-                              title: "Task Type",
-                              icon: Icons.task),
-                          IconsContainer(
-                              onPressed: () {
-                                showDurationPicker(context);
-                              },
-                              title: "Duration",
-                              icon: Icons.timer),
-                        ],
-                      ),
-                    ),
-                    const SizedSpace(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          child: const Text(
-                            "Add",
-                            style: TextStyle(color: Colors.red),
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              tasks.add(
-                                TimePlannerTask(
-                                  color: colorMap[mainColour]!,
-                                  minutesDuration: timeMap[firstTime]!,
-                                  dateTime: TimePlannerDateTime(
-                                      day: dayMapping[dayOfTheWeek] ?? 2,
-                                      //day: 0,
-                                      hour: _dateTime.hour,
-                                      minutes: _dateTime.minute),
-                                  child: Text(controller.text),
-                                ),
-                              );
-                            });
-                          },
                         ),
-                        TextButton(
-                          child: Text("Dismiss",
-                              style: TextStyle(color: Colors.grey[700])),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        )
-                      ],
-                    ),
-                  ]),
+                        const Center(
+                          child: Text(
+                            "Add a Task",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: LongTextFieldForm(
+                            onChanged: (value) {},
+                            hintText: 'Enter Task',
+                            labelText: 'Enter task',
+                            showSuffixIcon: false,
+                            showPrefixIcon: true,
+                            prefixIcon: Icons.pending_actions,
+                            validator: (value) {
+                              Validation.usernameValidation(value);
+                            },
+                            obsureText: false,
+                          ),
+                        ),
+                        const SizedSpace(),
+                        const Text("Choose a date & Time",
+                            style: TextStyle(fontSize: 15)),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconsContainer(
+                                onPressed: () {
+                                  showDatePicker(context, setState);
+                                },
+                                title: 'Date',
+                                icon: Icons.date_range,
+                              ),
+                              IconsContainer(
+                                  onPressed: () {
+                                    showTimePicker(context);
+                                  },
+                                  title: "Time",
+                                  icon: Icons.timelapse),
+                            ],
+                          ),
+                        ),
+                        const Text("Task Type & Duration"),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconsContainer(
+                                  onPressed: () {
+                                    showTaskPicker(context, setState);
+                                  },
+                                  title: "Task Type",
+                                  icon: Icons.task),
+                              IconsContainer(
+                                  onPressed: () {
+                                    showDurationPicker(context);
+                                  },
+                                  title: "Duration",
+                                  icon: Icons.timer),
+                            ],
+                          ),
+                        ),
+                        const SizedSpace(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              child: const Text(
+                                "Add",
+                                style: TextStyle(color: Colors.red),
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  tasks.add(
+                                    TimePlannerTask(
+                                      color: selectedTaskColor,
+                                      minutesDuration: timeMap[firstTime]!,
+                                      dateTime: TimePlannerDateTime(
+                                          day: dayMapping[selectedDayOfTheWeek ?? dayOfTheWeek] ?? 2,
+                                          //day: 0,
+                                          hour: _dateTime.hour,
+                                          minutes: _dateTime.minute),
+                                      child: Text(controller.text),
+                                    ),
+                                  );
+                                });
+                              },
+                            ),
+                            TextButton(
+                              child: Text("Dismiss",
+                                  style: TextStyle(color: Colors.grey[700])),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            )
+                          ],
+                        ),
+                      ]);
+                },
+              ),
             ),
           );
         });
@@ -255,7 +261,9 @@ class _TimetablePageState extends State<TimetablePage> {
   showTimePicker(BuildContext context) {
     showDialog(
         context: context,
-        builder: (BuildContext context) {
+        builder: (
+          BuildContext context,
+        ) {
           return AlertDialog(
             title: const Text("select a time"),
             content: TimePickerSpinner(
@@ -278,54 +286,76 @@ class _TimetablePageState extends State<TimetablePage> {
                   onPressed: () {
                     Navigator.pop(context);
                   },
-                  child:  const Text(
-                  "Done",
-                  style: TextStyle(color: Colors.red),))
+                  child: const Text(
+                    "Done",
+                    style: TextStyle(color: Colors.red),
+                  ))
             ],
           );
         });
   }
 
   ///datePicker
-  showDatePicker(BuildContext context) {
+  void showDatePicker(BuildContext context, StateSetter parentSetState) {
+    List<String> daysOfTheWeek = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
     int selectedIndex = 0;
-    int tempSlectedIndex = selectedIndex;
+
     showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Center(child: Text("Select a date")),
-            content: SizedBox(
-              height: 120,
-              child: ListWheelScrollView(
-                itemExtent: 30,
-                useMagnifier: true,
-                magnification: 1.5,
-                onSelectedItemChanged: (index) {
-                  setState(() {
-                    tempSlectedIndex = index;
-                  });
-                },
-                children: daysOfTheWeek
-                    .map((day) => Center(child: Text(day)))
-                    .toList(),
-              ),
-            ),
-            actions: [
-              TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Center(child: Text("Select a date")),
+          content: SizedBox(
+            height: 120,
+            child: StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+                return ListWheelScrollView(
+                  itemExtent: 30,
+                  useMagnifier: true,
+                  magnification: 1.5,
+                  onSelectedItemChanged: (index) {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                    parentSetState(() {
+                      selectedDayOfTheWeek = daysOfTheWeek[selectedIndex];
+                    });
                   },
-                  child:  const Text(
-                  "Done",
-                  style: TextStyle(color: Colors.red),))
-            ],
-          );
-        });
+                  children: daysOfTheWeek
+                      .map((day) => Center(child: Text(day)))
+                      .toList(),
+                );
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text(
+                "Done",
+                style: TextStyle(color: Colors.red),
+              ),
+            )
+          ],
+        );
+      },
+    );
   }
 
+
   ///taskPicker
-  showTaskPicker(BuildContext context) {
+  ///Todo: It does work but there is something wrong that needs you to comeback to it
+  showTaskPicker(BuildContext context, StateSetter parentSetState) {
     List<String> tasks = [
       "Important",
       "Urgent",
@@ -356,6 +386,9 @@ class _TimetablePageState extends State<TimetablePage> {
                     setState(() {
                       selectedIndex = index;
                     });
+                    parentSetState(() {
+                      selectedTaskColor = taskColors[selectedIndex]!;
+                    });
                   },
                   children: tasks
                       .asMap()
@@ -383,13 +416,13 @@ class _TimetablePageState extends State<TimetablePage> {
           ),
           actions: [
             TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child:  const Text(
-              "Done",
-              style: TextStyle(color: Colors.red),)
-            )
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  "Done",
+                  style: TextStyle(color: Colors.red),
+                ))
           ],
         );
       },
@@ -440,8 +473,9 @@ class _TimetablePageState extends State<TimetablePage> {
                     Navigator.pop(context);
                   },
                   child: const Text(
-                  "Done",
-                  style: TextStyle(color: Colors.red),))
+                    "Done",
+                    style: TextStyle(color: Colors.red),
+                  ))
             ],
           );
         });
