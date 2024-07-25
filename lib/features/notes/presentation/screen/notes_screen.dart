53d282a955aa6df7cc5bea7186f59f7908cba8f6
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-25 15:11:34 +0200

    created notes screen

--- lib/features/notes/presentation/screen/notes_screen.dart
+++ lib/features/notes/presentation/screen/notes_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mylearning/common_widgets/screens/appBar_layout/app_bar_screen.dart';
 
 class NotesScreen extends StatefulWidget {
   const NotesScreen({super.key});
@@ -8,8 +9,27 @@ class NotesScreen extends StatefulWidget {
 }
 
 class _NotesScreenState extends State<NotesScreen> {
+  final TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppBarScreen(
+      shouldScroll: true,
+      title: "My Notes",
+      shouldHaveFloatingButton: true,
+      shouldBeCentered: true,
+      icon: Icons.add,
+      floatingActionButton: () => _showInputAlert(context),
+      children: [
+      ],
+    );
+  }
+
+  _showInputAlert(BuildContext context) {
+    return showDialog(context: context, builder: (BuildContext context) {
+      return AlertDialog();
+    });
   }
 }
+
+
