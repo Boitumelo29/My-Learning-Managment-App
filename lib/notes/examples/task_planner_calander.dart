c2800f31a5edff92128ef82452396a1a45847c88
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-29 08:57:12 +0200

    update

---
+++ lib/notes/examples/task_planner_calander.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TodoModel()),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo App with Calendar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoScreen(),
+    );
+  }
+}
+
+class Todo {
+  final String title;
+  final DateTime date;
+
+  Todo({
+    required this.title,
+    required this.date,
+  });
+}
+
+class TodoModel extends ChangeNotifier {
+  List<Todo> _todos = [];
+
+  List<Todo> get todos => _todos;
+
+  void addTodo(String title, DateTime date) {
+    _todos.add(Todo(
+      title: title,
+      date: date,
+    ));
+    notifyListeners();
+  }
+}
+
+class TodoScreen extends StatefulWidget {
+  @override
+  _TodoScreenState createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  late CalendarController _calendarController;
+  DateTime? _selectedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ToDo App with Calendar'),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            calendarController: _calendarController,
+            onDaySelected: (date, events, holidays) {
+              _selectDate(context, date);
+            }, focusedDay: null, firstDay: null, lastDay: null,
+          ),
+          Expanded(
+            child: Consumer<TodoModel>(
+              builder: (context, todoModel, child) {
+                final todos = _selectedDate == null
+                    ? todoModel.todos
+                    : todoModel.todos
+                        .where((todo) => isSameDay(todo.date, _selectedDate!))
+                        .toList();
+
+                return ListView.builder(
+                  itemCount: todos.length,
+                  itemBuilder: (context, index) {
+                    final todo = todos[index];
+                    return ListTile(
+                      title: Text(todo.title),
+                      subtitle: Text(todo.date.toIso8601String()),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addTodoDialog(context);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _selectDate(BuildContext context, DateTime date) {
+    setState(() {
+      _selectedDate = date;
+    });
+  }
+
+  Future<void> _addTodoDialog(BuildContext context) async {
+    final TextEditingController titleController = TextEditingController();
+    DateTime? selectedDate;
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add ToDo'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(hintText: 'Enter title'),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    final DateTime? picked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2020),
+                      lastDate: DateTime(2101),
+                    );
+                    if (picked != null && picked != selectedDate) {
+                      setState(() {
+                        selectedDate = picked;
+                      });
+                    }
+                  },
+                  child: Text('Select date'),
+                ),
+                if (selectedDate != null)
+                  Text('Selected date: ${selectedDate!.toLocal()}')
+                else
+                  Text('No date selected'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Add'),
+              onPressed: () {
+                if (titleController.text.isNotEmpty && selectedDate != null) {
+                  Provider.of<TodoModel>(context, listen: false)
+                      .addTodo(titleController.text, selectedDate!);
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  bool isSameDay(DateTime date1, DateTime date2) {
+    return date1.year == date2.year &&
+        date1.month == date2.month &&
+        date1.day == date2.day;
+  }
+}
