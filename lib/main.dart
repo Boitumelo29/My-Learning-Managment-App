e88c64fd5c33c350ec364400a1c3084a574dc0ab
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-29 11:07:26 +0200

    update, fixed all errors

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ import 'package:mylearning/util/navigation/tab_bar.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<NoteProvider>(create: (context) => NoteProvider()),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -19,22 +24,19 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<NoteProvider>(
-      create: (context) => NoteProvider(),
-      child: MaterialApp(
-        // theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
-        themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
-        theme: ThemeData(
-            scaffoldBackgroundColor: Colors.grey[50],
-            primaryColor: Colors.red,
-            primaryColorLight: Colors.red,
-            primaryColorDark: Colors.red[900]),
-        //theme: ThemeData(primarySwatch: Colors.red, brightness: Brightness.light),
-        darkTheme:
-            ThemeData(primarySwatch: Colors.red, brightness: Brightness.dark),
-        debugShowCheckedModeBanner: false,
-        home: TabBarScreen(isDarkMode: isDarkMode, toggleTheme: toggleTheme),
-      ),
+    return MaterialApp(
+      // theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.grey[50],
+          primaryColor: Colors.red,
+          primaryColorLight: Colors.red,
+          primaryColorDark: Colors.red[900]),
+      //theme: ThemeData(primarySwatch: Colors.red, brightness: Brightness.light),
+      darkTheme:
+          ThemeData(primarySwatch: Colors.red, brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
+      home: TabBarScreen(isDarkMode: isDarkMode, toggleTheme: toggleTheme),
     );
   }
 
--- lib/notes/examples/task_planner_calander.dart
+++ lib/notes/examples/task_planner_calander.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter/foundation.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 void main() {
@@ -49,6 +48,10 @@ class TodoModel extends ChangeNotifier {
     ));
     notifyListeners();
   }
+
+  List<DateTime> get todoDates {
+    return _todos.map((todo) => todo.date).toList();
+  }
 }
 
 class TodoScreen extends StatefulWidget {
@@ -68,36 +71,77 @@ class _TodoScreenState extends State<TodoScreen> {
       ),
       body: Column(
         children: [
-          TableCalendar(
-            firstDay: DateTime(2020),
-            lastDay: DateTime(2100),
-            focusedDay: _focusedDay,
-            selectedDayPredicate: (day) {
-              return _selectedDay != null ? isSameDay(_selectedDay, day) : false;
-            },
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay; // update `_focusedDay` here as well
-              });
+          Consumer<TodoModel>(
+            builder: (context, todoModel, child) {
+              return TableCalendar(
+                firstDay: DateTime(2020),
+                lastDay: DateTime(2100),
+                focusedDay: _focusedDay,
+                selectedDayPredicate: (day) {
+                  return _selectedDay != null ? isSameDay(_selectedDay!, day) : false;
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay; // update `_focusedDay` here as well
+                  });
+                },
+                calendarBuilders: CalendarBuilders(
+                  defaultBuilder: (context, day, focusedDay) {
+                    if (todoModel.todoDates.any((todoDate) => isSameDay(todoDate, day))) {
+                      return Container(
+                        margin: const EdgeInsets.all(4.0),
+                        decoration: BoxDecoration(
+                          color: Colors.blue[300],
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '${day.day}',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      );
+                    }
+                    return null;
+                  },
+                ),
+              );
             },
           ),
           Expanded(
             child: Consumer<TodoModel>(
               builder: (context, todoModel, child) {
-                final todos = _selectedDay == null
-                    ? todoModel.todos
-                    : todoModel.todos
-                    .where((todo) => isSameDay(todo.date, _selectedDay!))
-                    .toList();
-
                 return ListView.builder(
-                  itemCount: todos.length,
+                  itemCount: todoModel.todos.length,
                   itemBuilder: (context, index) {
-                    final todo = todos[index];
-                    return ListTile(
-                      title: Text(todo.title),
-                      subtitle: Text(todo.date.toIso8601String()),
+                    final todo = todoModel.todos[index];
+                    return Card(
+                      margin: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        leading: Container(
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: Text(
+                            '${todo.date.day} ${_getMonth(todo.date.month)}',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        title: Text(todo.title),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('${todo.date.hour}:${todo.date.minute} pm'),
+                            Text('Online'),
+                          ],
+                        ),
+                      ),
                     );
                   },
                 );
@@ -123,56 +167,60 @@ class _TodoScreenState extends State<TodoScreen> {
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Add ToDo'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                TextField(
-                  controller: titleController,
-                  decoration: InputDecoration(hintText: 'Enter title'),
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Add ToDo'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(hintText: 'Enter title'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        final DateTime? picked = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2020),
+                          lastDate: DateTime(2101),
+                        );
+                        if (picked != null) {
+                          setState(() {
+                            selectedDate = picked;
+                          });
+                        }
+                      },
+                      child: Text('Select date'),
+                    ),
+                    if (selectedDate != null)
+                      Text('Selected date: ${selectedDate!.toLocal()}')
+                    else
+                      Text('No date selected'),
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: () async {
-                    final DateTime? picked = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(2020),
-                      lastDate: DateTime(2101),
-                    );
-                    if (picked != null && picked != selectedDate) {
-                      setState(() {
-                        selectedDate = picked;
-                      });
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: Text('Add'),
+                  onPressed: () {
+                    if (titleController.text.isNotEmpty && selectedDate != null) {
+                      Provider.of<TodoModel>(context, listen: false)
+                          .addTodo(titleController.text, selectedDate!);
+                      Navigator.of(context).pop();
                     }
                   },
-                  child: Text('Select date'),
                 ),
-                if (selectedDate != null)
-                  Text('Selected date: ${selectedDate!.toLocal()}')
-                else
-                  Text('No date selected'),
               ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Add'),
-              onPressed: () {
-                if (titleController.text.isNotEmpty && selectedDate != null) {
-                  Provider.of<TodoModel>(context, listen: false)
-                      .addTodo(titleController.text, selectedDate!);
-                  Navigator.of(context).pop();
-                }
-              },
-            ),
-          ],
+            );
+          },
         );
       },
     );
@@ -183,5 +231,12 @@ class _TodoScreenState extends State<TodoScreen> {
         date1.month == date2.month &&
         date1.day == date2.day;
   }
-}
 
+  String _getMonth(int month) {
+    const months = [
+      'January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    return months[month - 1];
+  }
+}
