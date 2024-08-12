93ea00f0ad831a2dab3111acdd534fc114168570
Boitumelo Thobejane <tumijane99@gmail.com>
2024-08-12 08:45:23 +0200

    update

--- lib/common_widgets/widgets/containers/auth_container.dart
+++ lib/common_widgets/widgets/containers/auth_container.dart
@@ -2,18 +2,40 @@ import 'package:flutter/material.dart';
 
 class AuthContainer extends StatelessWidget {
   final String title;
+  final IconData iconData;
   final Function() onPressed;
 
   const AuthContainer(
-      {super.key, required this.title, required this.onPressed});
+      {super.key,
+      required this.title,
+      required this.onPressed,
+      required this.iconData});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: Container(
-        child: Text(title),
-      ),
       onTap: onPressed,
+      child: Container(
+        height: 40,
+        width: 60,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(width: 1, color: Colors.grey)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(
+              iconData,
+              color: Colors.grey,
+            ),
+            Text(
+              title,
+              style: const TextStyle(color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
