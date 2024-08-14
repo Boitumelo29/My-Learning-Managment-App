5b726bf0fa82ce3b6ff369542fd70dd589d953b1
Boitumelo Thobejane <tumijane99@gmail.com>
2024-08-14 11:06:23 +0200

    update

---
+++ lib/common_widgets/widgets/cards/expansion_faq_tile.dart
@@ -0,0 +1,29 @@
+import 'package:expansion_tile_card/expansion_tile_card.dart';
+import 'package:flutter/material.dart';
+
+class ExpansionFaqTile extends StatelessWidget {
+  final GlobalKey<ExpansionTileCardState> card;
+  final String title;
+  final String subTitle;
+  final List<Widget> children;
+
+  const ExpansionFaqTile(
+      {super.key,
+      required this.card,
+      required this.title,
+      required this.subTitle,
+      required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTileCard(
+        key: card,
+        leading: const CircleAvatar(
+          backgroundColor: Colors.red,
+          child: Text("FAQ"),
+        ),
+        title: Text(title),
+        subtitle: const Text("What do we do?"),
+        children: children);
+  }
+}
