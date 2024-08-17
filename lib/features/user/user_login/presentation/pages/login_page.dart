75fa08628286457fecb2101b005a7ab228f7ad05
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2024-08-17 20:29:42 +0200

    fixed forget password

--- lib/features/user/user_login/presentation/pages/login_page.dart
+++ lib/features/user/user_login/presentation/pages/login_page.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:mylearning/common_widgets/screens/user_layout/user_layout_screen.dart';
 import 'package:mylearning/common_widgets/sized_box/sized_space.dart';
 import 'package:mylearning/common_widgets/widgets/buttons/long_button.dart';
-import 'package:mylearning/common_widgets/widgets/textfield/textfields.dart';
 import 'package:mylearning/features/user/user_login/presentation/widget/auth_row.dart';
 import 'package:mylearning/features/user/user_login/presentation/widget/email.dart';
+import 'package:mylearning/features/user/user_login/presentation/widget/fogotpassword.dart';
 import 'package:mylearning/features/user/user_login/presentation/widget/forget_password.dart';
 import 'package:mylearning/features/user/user_login/presentation/widget/have_an_account.dart';
 import 'package:mylearning/features/user/user_login/presentation/widget/logo_image.dart';
@@ -49,13 +49,15 @@ class _LoginPageState extends State<LoginPage> {
                   email: email,
                   emailFocus: emailFocus,
                 ),
-                const SizedBox(height: 10,),
+                const SizedBox(
+                  height: 10,
+                ),
                 LoginPassword(
                   password: password,
                   passwordFocus: passwordFocus,
                 ),
                 LoginForgetPassword(onTap: () {
-                  forgotPassword();
+                  forgotPassword(context, setState, isForgotPasswordLoading);
                 }),
                 LongButton(
                   isLoading: isLoading,
@@ -115,106 +117,6 @@ class _LoginPageState extends State<LoginPage> {
     } finally {
       setState(() {
         isLoading = false;
-      });
-    }
-  }
-
-  forgotPassword() {
-    TextEditingController resetPasswordController = TextEditingController();
-
-    showModalBottomSheet(
-        isScrollControlled: true,
-        context: context,
-        builder: (BuildContext context) {
-          return Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: SizedBox(
-              height: 240,
-              width: 480,
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Merriweather',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    "No Worries, we'll send you reset instructions",
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  LongTextFieldForm(
-                    controller: resetPasswordController,
-                    onChanged: (value) {},
-                    hintText: "Please enter your email address",
-                    labelText: "Email",
-                    showSuffixIcon: false,
-                    showPrefixIcon: true,
-                    prefixIcon: Icons.email,
-                    validator: (value) {},
-                    obsureText: false,
-                    isRed: false,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  LongButton(
-                      isLoading: isForgotPasswordLoading,
-                      onTap: () async {
-                        await resetPassword(resetPasswordController.text);
-                        print(resetPasswordController.text);
-                      },
-                      title: "Reset Password"),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                ],
-              ),
-            ),
-          );
-        });
-  }
-
-  Future<void> resetPassword(String email) async {
-    setState(() {
-      isForgotPasswordLoading = true;
-    });
-    try {
-      print("I am trying");
-      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text(
-            " Reset instructions have been sent to $email, please check your emails"),
-        backgroundColor: Colors.yellow,
-        duration: const Duration(seconds: 5),
-      ));
-      setState(() {
-        isForgotPasswordLoading = false;
-      });
-    } catch (e) {
-      print(e);
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("Oops and error has occurred: $e"),
-        backgroundColor: Colors.red,
-        duration: const Duration(seconds: 5),
-      ));
-      setState(() {
-        isForgotPasswordLoading = false;
-      });
-    } finally {
-      setState(() {
-        isForgotPasswordLoading = false;
       });
     }
   }
