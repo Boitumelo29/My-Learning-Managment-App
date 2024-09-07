a612390f9de584557267176f65fbeadcc98d27b5
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2024-09-07 19:10:45 +0200

    work in progress: added upcoming events

--- lib/features/user/user_sign_up/page/sign_up_page.dart
+++ lib/features/user/user_sign_up/page/sign_up_page.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mylearning/common_widgets/screens/user_layout/user_layout_screen.dart';
