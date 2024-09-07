d6ed6e4ee3e056bc30980c79d10eaefa0bad39e1
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2024-09-07 18:22:53 +0200

    work in progress: added upcoming events

--- lib/features/home/home_screen/widgets/upcoming events.dart
+++ lib/features/home/home_screen/widgets/upcoming events.dart
@@ -11,66 +11,52 @@ class UpcomingEvents extends StatelessWidget {
         ScaffoldMessenger.of(context)
             .showSnackBar(const SnackBar(content: Text("action")));
       },
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            height: 120,
-            width: 400,
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                //boxShadow: ,
-                borderRadius: BorderRadius.circular(20)),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.red.shade50,
-                borderRadius: BorderRadius.circular(20)),
-            height: 100,
-            width: 370,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        height: 100,
+        width: 370,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.arrow_right,
+                      color: Colors.red,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        width: 2,
-                        height: 60,
-                        color: Colors.red,
+                      SizedBox(
+                        height: 15,
                       ),
-                      const SizedBox(
-                        width: 5,
+                      Text("My Todo"),
+                      SizedBox(
+                        height: 10,
                       ),
-                      const Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Text("My Todo"),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text("09:00 - 10:30"),
-                        ],
-                      )
+                      Text("09:00 - 10:30"),
                     ],
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Icon(
-                    Icons.arrow_right,
-                    color: Colors.red,
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
-          )
-        ],
+            Container(
+              width: 2,
+              height: 70,
+              color: Colors.red,
+            ),
+          ],
+        ),
       ),
     );
   }
