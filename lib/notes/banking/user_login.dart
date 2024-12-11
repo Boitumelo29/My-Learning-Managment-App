56e880832d1a7b85295ba726188f7d7eb3fdc2f1
Boitumelo Thobejane <tumijane99@gmail.com>
2024-12-11 13:39:52 +0200

    upd

---
+++ lib/notes/banking/user_login.dart
@@ -0,0 +1,99 @@
+// import 'package:flutter/material.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Simple Login App',
+//       home: FutureBuilder(
+//         future: checkLoginStatus(),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.done) {
+//             bool isLoggedIn = snapshot.data as bool;
+//             return isLoggedIn ? HomeScreen() : LoginPage();
+//           }
+//           return Scaffold(body: Center(child: CircularProgressIndicator()));
+//         },
+//       ),
+//     );
+//   }
+//
+//   Future<bool> checkLoginStatus() async {
+//     final SharedPreferences prefs = await SharedPreferences.getInstance();
+//     return prefs.getBool('isLoggedIn') ?? false;
+//   }
+// }
+//
+// class LoginPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Login')),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             TextFormField(
+//               decoration: InputDecoration(
+//                 labelText: 'Email',
+//                 border: OutlineInputBorder(),
+//               ),
+//               keyboardType: TextInputType.emailAddress,
+//             ),
+//             SizedBox(height: 20),
+//             TextFormField(
+//               decoration: InputDecoration(
+//                 labelText: 'Password',
+//                 border: OutlineInputBorder(),
+//               ),
+//               obscureText: true,
+//             ),
+//             SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: () => loginUser(context),
+//               child: Text('Login'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Future<void> loginUser(BuildContext context) async {
+//     final SharedPreferences prefs = await SharedPreferences.getInstance();
+//     await prefs.setBool('isLoggedIn', true);
+//     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+//   }
+// }
+//
+// class HomeScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Home'),
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.exit_to_app),
+//             onPressed: () => logoutUser(context),
+//           ),
+//         ],
+//       ),
+//       body: Center(
+//         child: Text('Welcome to the Home Page!', style: TextStyle(fontSize: 24)),
+//       ),
+//     );
+//   }
+//
+//   Future<void> logoutUser(BuildContext context) async {
+//     final SharedPreferences prefs = await SharedPreferences.getInstance();
+//     await prefs.remove('isLoggedIn');
+//     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+//   }
+// }
